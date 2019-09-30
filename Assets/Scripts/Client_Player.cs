07a0109bba554e9f50937ac9a14fc9116eb93511
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2019-09-30 14:51:07 +0200

    fix lerp

---
+++ Assets/Scripts/Client_Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client_Player : MonoBehaviour
+{
+    Vector2 velocity;
+    Rigidbody2D rigidbody;
+    public Vector2 serverPos;
+    public float serverSpeed;
+
+    Vector3 offset = new Vector3(1.0f,1.0f,0.0f);
+    [SerializeField]Server_Player server_player;
+    float timer;
+    const float period = 0.1f;
+
+    public List<Vector2> serverPositions = new List<Vector2>(2);
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        timer = 0;
+    }
+
+    void Update()
+    {
+        //Transport
+        timer += Time.deltaTime;
+
+        if(timer >= period)
+        {
+            server_player.StartCoroutine(server_player.SendDatas());
+            timer = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(serverPositions[0],serverPositions[1], Time.deltaTime * serverSpeed);
+
+        var dir = server_player.GetVelocity();
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rigidbody.MoveRotation(angle);
+    }
+}
---
+++ Assets/Scripts/Server_Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Server_Player : MonoBehaviour
+{
+    float speed;
+    Vector2 velocity;
+    Rigidbody2D rigidbody;
+    Vector2 position;
+    [SerializeField]Client_Player client_player;
+
+    const float transportDelay = 0.1f;
+    float randomSpeed;
+    float randomVelocity;
+    float randomDirectionDelay;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        StartCoroutine(ChangeDirection());
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(velocity.x * speed, velocity.y * speed);
+
+        var dir = rigidbody.velocity;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rigidbody.MoveRotation(angle);
+    }
+
+    IEnumerator ChangeDirection()
+    {
+        randomSpeed = Random.Range(1, 5);
+        randomVelocity = Random.Range(-1, 2);
+        randomDirectionDelay = Random.Range(1, 3);
+        speed = randomSpeed;
+        velocity.x = randomVelocity;
+        velocity.y = randomVelocity;
+        yield return new WaitForSeconds(randomDirectionDelay);
+        StartCoroutine(ChangeDirection());
+    }
+
+     public IEnumerator SendDatas()
+    {
+        yield return new WaitForSeconds(transportDelay);//Emission
+        position = transform.position;
+        client_player.serverSpeed = speed;
+        if(client_player.serverPositions.Count >= 2)
+        {
+            client_player.serverPositions.Remove(client_player.serverPositions[0]);
+        }
+        client_player.serverPositions.Add(position);
+    }
+
+    public Vector2 GetVelocity()
+    {
+        return rigidbody.velocity;
+    }
+
+
+}
